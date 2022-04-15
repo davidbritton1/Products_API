de2bb4509db12084bb6cef1148356edcdf6591e3
davidbritton1 <102608670+davidbritton1@users.noreply.github.com>
2022-04-15 01:00:25 -0700

    Completed Functionality for Products Views File

--- products/views.py
+++ products/views.py
@@ -1,12 +1,9 @@
-from itertools import product
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
-import products
 from .serializers import ProductSerializer
 from .models import Products
-from products import serializers
 
 @api_view(['GET', 'POST'])
 def product_list(request):
@@ -22,12 +19,17 @@ def product_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
       
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
-    try:
-        product = Products.objects.get(pk=pk)
+    product = get_object_or_404(Products, pk=pk)
+    if request.method == 'GET':
         serializers = ProductSerializer(product);
         return Response(serializers.data)
-
-    except Products.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND);
+    elif request.method == 'PUT':
+        serializers = ProductSerializer(product, data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save()
+        return Response(serializers.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
