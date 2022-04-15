1470940abfe37d67b924deac1863a18f5037ddd3
davidbritton1 <102608670+davidbritton1@users.noreply.github.com>
2022-04-15 00:28:58 -0700

    Created New View Function + Plus added New URLs Path For Products

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
+from urllib import request
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.product_list),
+    path('<int:pk>/', views.product_detail),
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
+from itertools import product
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+import products
 from .serializers import ProductSerializer
 from .models import Products
+from products import serializers
 
 @api_view(['GET', 'POST'])
 def product_list(request):
@@ -18,3 +22,12 @@ def product_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
       
+@api_view(['GET'])
+def product_detail(request, pk):
+    try:
+        product = Products.objects.get(pk=pk)
+        serializers = ProductSerializer(product);
+        return Response(serializers.data)
+
+    except Products.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);
